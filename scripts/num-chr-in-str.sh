3b907ccc04294bddc89589c7fe2545d560c786f8
Jacobs <yakovsobolev1@gmail.com>
2024-05-15 17:19:44 +0300

    script that count the number of latters in str

---
+++ scripts/num-chr-in-str.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+########################
+# Script that counts the letters 
+# of $str 
+# --------
+# grep -o => returns each latter in a new line 
+# wc = word count , wc -l => counts the lines
+# <<<  -  here is string
+########################
+
+str=mississippi
+char=s
+
+echo $str | grep -o $char | wc -l
+# grep -o $char <<<"$str" | wc -l # alternative way
